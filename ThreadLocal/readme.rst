f035339197e9cec9fa091d4e52ea596c97ba28ca
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-05-22 10:07:58 +0530

    Add files via upload

---
+++ ThreadLocal/readme.rst
@@ -0,0 +1,1 @@
+You can read tutorial https://www.jeejava.com/how-threadlocal-works-in-java/
---
+++ ThreadLocal/src/main/java/com/jeejava/threadlocal/ThreadLocalTest.java
@@ -0,0 +1,13 @@
+package com.jeejava.threadlocal;
+
+public class ThreadLocalTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread t1 = new Thread(new TaskThread());
+		Thread t2 = new Thread(new TaskThread());
+
+		t1.start();
+		t2.start();
+	}
+
+}
