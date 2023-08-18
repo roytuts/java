00489a4f992174fba878fd5c63cb6775d1d57ff6
Soumitra <projwebinfo@gmail.com>
2023-08-18 18:48:30 +0530

    Create ShallowCopyCloneTest.java

---
+++ java-shallow-deep-copy-clone/src/main/java/com/roytuts/java/shallowdeep/copyclone/ShallowCopyCloneTest.java
@@ -0,0 +1,27 @@
+package com.roytuts.java.shallowdeep.copyclone;
+
+import com.roytuts.java.shallowdeep.copyclone.vo.Department;
+import com.roytuts.java.shallowdeep.copyclone.vo.Employee;
+
+public class ShallowCopyCloneTest {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+		Employee employee = new Employee("Soumitra");
+		Department department = new Department("Computer", employee);
+
+		Department clonedDept = (Department) department.clone();
+
+		System.out.println("Employee name in department : " + department.getEmployee().getName());
+		System.out.println("Employee name in clonedDept : " + clonedDept.getEmployee().getName());
+
+		System.out.println();
+		System.out.println("After setting Employee name to cloned object");
+		System.out.println();
+
+		clonedDept.getEmployee().setName("Soumitra2");
+
+		System.out.println("After setting, Employee name in clonedDept : " + clonedDept.getEmployee().getName());
+		System.out.println("After setting, Employee name in department : " + department.getEmployee().getName());
+	}
+
+}
