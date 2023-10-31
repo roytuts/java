2190a50e29d2f9b1a519b36b1877e060f5a7269c
Soumitra <projwebinfo@gmail.com>
2023-10-31 16:33:57 +0530

    Update BatchFile.java

--- java-batch-file/src/main/java/com/roytuts/java/batch/file/BatchFile.java
+++ java-batch-file/src/main/java/com/roytuts/java/batch/file/BatchFile.java
@@ -8,25 +8,40 @@ public class BatchFile {
 
 	public static void main(String[] args) {
 		try {
-			// create new file called sample in "d" drive
-			File file = new File("sample.bat");
+			final String fileName = "sample.bat";
+			final String filePath = "C:/eclipse-workspace";
+			
+			// create new file called sample.bat
+			File file = new File(filePath + File.separator + fileName);
+			
 			FileOutputStream fos = new FileOutputStream(file);
 
 			// write some commands to the file
 			DataOutputStream dos = new DataOutputStream(fos);
 			dos.writeBytes("cd \\");
 			dos.writeBytes("\n");
-			dos.writeBytes("echo %path%");
+			dos.writeBytes("%path%");
 			dos.writeBytes("\n");
+			dos.close();
 
 			// execute the batch file
-			Process p = Runtime.getRuntime().exec("cmd /c start sample.bat");
-
+			//Process p = Runtime.getRuntime().exec("cmd /c start sample.bat"); // The method exec(String) from the type
+																				// Runtime is deprecated since version
+																				// 18
+			
+			//Java 19 fix
+			//String[] command = {"cmd.exe", "/c", "Start", filePath+File.separator+fileName};
+            //Process p =  Runtime.getRuntime().exec(command);
+            
+			//Helps in debugging if any issue occurs
+			ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "start", fileName);
+			pb.directory(new File(filePath));
+			Process p = pb.start();
+			
 			// wait for termination
-			p.waitFor();
-
-			dos.close();
+			p.waitFor();			
 		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
 	}
 
